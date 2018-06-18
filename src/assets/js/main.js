e28f85c1c0c6326560534567ebd01a365da464a1
RavAlmasy <eb.vega.m@gmail.com>
2018-06-18 18:45:20 -0400

    arreglado menu lateral que no escondía los cohorts al hacer click en otra opción, además coloqué la sidebar y el header en fixed para que no  desaparaezca al hacer scroll a la página

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,53 +1,37 @@
+const asideBtn = document.getElementById('expandMenu')
 const usersBtn = document.getElementById('show');
 const tableName = document.getElementById('studentsName');
 const hiddingElement = document.getElementById('mainInfo');
 const usersJSON = '../../../data/cohorts/lim-2018-03-pre-core-pw/users.json';
 
 fetch(usersJSON)
-.then(response => response.json())
-.then(data => {
-  console.log(data);
-  renderUsers(data);
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    renderUsers(data);
+  })
+
+asideBtn.addEventListener('click', () => {
+  const logo = document.getElementById('logo');
+  const sidebar = document.getElementById('sidebar');
+  if (logo.style.display === 'none') {
+    logo.style.display = 'block';
+  } else if (logo.style.display === 'block') {
+    logo.style.display = 'none';
+  } else if (sidebar.style.display === 'none') {
+    sidebar.style.display = 'block';
+  } else if (sidebar.style.display === 'block') {
+    sidebar.style.display = 'none';
+  }
 })
+
 
 const renderUsers = data => {
   usersBtn.addEventListener('click', () => {
     hiddingElement.classList.remove('d-none');
     const render = data.forEach(element => {
-      return tableName.innerHTML += `<p>${element.name}</p>` 
+      return tableName.innerHTML += `<p>${element.name.toUpperCase()}</p>`
     })
     return render;
   })
 }
-
-/*
-Para crear tablas dinámicas
-
-const show = (data) => {
-  const tblPlace = document.getElementsByClassName('table');
-  // Crear elemento tbody
-  let tblBody = document.querySelector('tbody');
-  // Crea las celdas
-  for (let i = 1; i < 5; i++) {
-    // Crea las hileras de la tabla
-    let row = document.createElement('tr');
- 
-    for (let j = 0; j < 2; j++) {
-      // Crea un elemento <td> y un nodo de texto, haz que el nodo de
-      // texto sea el contenido de <td>, ubica el elemento <td> al final
-      // de la hilera de la tabla
-      let cell = document.createElement('td');
-
-      const render = data.forEach(element => {
-        return row.innerHTML = `<td>${element.name}</td>`;
-      })
-
-      cell.appendChild(render);
-      row.appendChild(cell);
-    }
- 
-    // agrega la hilera al final de la tabla (al final del elemento tblbody)
-    tblBody.appendChild(row);
-  }
-}
-*/
