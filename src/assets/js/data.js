1b047732173d86d144859c93f29cabf107a9aa29
RavAlmasy <eb.vega.m@gmail.com>
2018-06-20 18:29:58 -0400

    trabajo en data.js, por el momento imprime en consola, lecturas totales, completadas y % y ejercicios totales, completados y %

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -1,33 +1,104 @@
-window.database = () => {
+window.onload = () => {
+  database();
+}
+
+let users = {};
+let cohorts = {};
+let progress = {};
+
+// Traer los archivos json
+const database = () => {
   const usersJSON = '../../../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+  const cohortsJSON = '../../../data/cohorts.json';
   const progressJSON = '../../../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
 
   fetch(usersJSON)
-  .then(response => response.json())
-  .then(users => {
-    console.log(users);
-    callingUsers(users);
-  })
+    .then(response => response.json())
+    .then(data => {
+      users = data;
+      console.log(users);
+    })
 
-fetch(progressJSON)
-  .then(response => response.json())
-  .then(progressJSON => {
-    console.log(progressJSON);
-    const completition = Object.entries(progressJSON);
-    console.log(completition);
-  })
+  fetch(cohortsJSON)
+    .then(response => response.json())
+    .then(cohortsJSON => {
+      cohort = cohortsJSON[31].coursesIndex;
+      console.log(cohort);
+    })
+
+  fetch(progressJSON)
+    .then(response => response.json())
+    .then(progressJSON => {
+      progress = progressJSON;
+      console.log(progress);
+    })
 }
 
-const callingUsers = (users) => {
+// Calcular lecturas
+window.getReadings = (users, progress) => {
+  let courses = Object.entries(progress).find(element => element[0] == users.id)[1];
+  console.log(courses);
+  let units = Object.entries(courses.intro.units);
+  console.log(units);
+  let readings = 0;
+  let completedReadings = 0;
+  for (let i = 0; i < units.length; i++) {
+    let parts = Object.entries(units[i][1].parts);
+    console.log(parts);
+    for (let j = 0; j < parts.length; j++) {
+      let chapter = parts[j][1];
+      console.log(chapter);
+      if (chapter.type == 'read') {
+        readings++;
+        console.log(readings);
+      }
+      if (chapter.type == 'read' && chapter.completed == 1) {
+        completedReadings++;
+        console.log(completedReadings);
+      }
+      console.log(`estas ${readings} y estas ${completedReadings}`);
+    }
+    let readingsPercent = (completedReadings / readings) * 100;
+    console.log(readingsPercent);
+  };
+}
 
-
+// Calcular ejercicios
+window.getExercizes = (users, progress) => {
+  let courses = Object.entries(progress).find(element => element[0] == users.id)[1];
+  console.log(courses);
+  let units = Object.entries(courses.intro.units);
+  console.log(units);
+  let exercizes = 0;
+  let completedExercizes = 0;
+  for (let i = 0; i < units.length; i++) {
+    let parts = Object.entries(units[i][1].parts);
+    console.log(parts);
+    for (let j = 0; j < parts.length; j++) {
+      let chapter = parts[j][1];
+      console.log(chapter);
+      if (chapter.type == 'practice') {
+        exercizes++;
+        console.log(exercizes);
+      }
+      if (chapter.type == 'practice' && chapter.completed == 1) {
+        completedExercizes++;
+        console.log(completedExercizes);
+      }
+      console.log(`estas ${exercizes} y estas ${completedExercizes}`);
+    }
+    let exercizesPercent = (completedExercizes / exercizes) * 100;
+    console.log(exercizesPercent);
+  };
 }
 
 
 window.computeUsersStats = (user, progress, courses) => {
-  completition.find(callback, [])  
-  
+  let userId = user.id;
+
+
 };
+
 window.sortUsers = (users, orderBy, orderDirection) => {
 
 };
@@ -37,3 +108,59 @@ window.filterUsers = (users, search) => {
 window.processCohortData = (options) => {
 
 };
+
+/*
+let studentInfo = user.forEach(element => {
+  let userName = element.name;
+  let userId = element.id;
+  let percent = 'Sin info';
+  if(progress[userId].intro) {
+    percent = progress[userId].intro.percent;
+  }
+
+
+
+let usersCohort = [];
+
+class User {
+  constructor(stats) {
+    this.stats = stats;
+  }
+
+
+
+    class Stats {
+      constructor(percent, practice, reads, quizzes) {
+        this.percent = percent;
+        this.practice = practice;
+        this.reads = reads;
+        this.quizzes = quizzes;
+      }
+    }
+
+
+class Exercizes {
+    constructor(total, completed, percent) {
+      this.total = total,
+      this.completed = completed,
+      this.percent = percent
+    }
+  }
+
+  class Reads {
+    constructor(total, completed, percent) {
+      this.total = total,
+      this.completed = completed,
+      this.percent = percent
+    }
+  }
+
+  class Quizzes {
+    constructor(total, completed, percent, scoreSum, scoreAvg) {
+      this.total = total,
+      this.completed = completed,
+      this.percent = percent,
+      this.scoreSum = scoreSum,
+      this.scoreAvg = scoreAvg
+    }
+  } */
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,29 +3,13 @@ const usersBtn = document.getElementById('show');
 const tableName = document.getElementById('studentsName');
 const hiddingElement = document.getElementById('mainInfo');
 
-/*
-asideBtn.addEventListener('click', () => {
-  const logo = document.getElementById('logo');
-  const sidebar = document.getElementById('sidebar');
-  if (logo.style.display === 'none') {
-    logo.style.display = 'block';
-  } else if (logo.style.display === 'block') {
-    logo.style.display = 'none';
-  } else if (sidebar.style.display === 'none') {
-    sidebar.style.display = 'block';
-  } else if (sidebar.style.display === 'block') {
-    sidebar.style.display = 'none';
-  }
-})
-*/
 
 // Proceso de impresión de los datos recibidos en una tabla 
 usersBtn.addEventListener('click', () => {
   hiddingElement.classList.remove('d-none');
 })
-
-
 /*
+
 const renderUsers = data => {
   let rankingNumber = 0;
   const render = data.forEach(element => {
@@ -41,4 +25,19 @@ const renderUsers = data => {
       '</tr>'
   })
 }
+
+
+asideBtn.addEventListener('click', () => {
+  const logo = document.getElementById('logo');
+  const sidebar = document.getElementById('sidebar');
+  if (logo.style.display === 'none') {
+    logo.style.display = 'block';
+  } else if (logo.style.display === 'block') {
+    logo.style.display = 'none';
+  } else if (sidebar.style.display === 'none') {
+    sidebar.style.display = 'block';
+  } else if (sidebar.style.display === 'block') {
+    sidebar.style.display = 'none';
+  }
+})
 */
