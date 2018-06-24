5d545c323ab06fe896422b2085e83c57a7fe7d95
RavAlmasy <eb.vega.m@gmail.com>
2018-06-23 22:08:52 -0400

    arreglo estilos css, un poco de responsive

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,21 +1,12 @@
-const asideBtn = document.getElementById('expandMenu');
+const asideBtn = document.getElementById('menu-toggle');
 const usersBtn = document.getElementById('show');
 const tableName = document.getElementById('studentsName');
 const hiddingElement = document.getElementById('mainInfo');
+const wrapper = document.getElementById('wrapper');
 
-
-/*
-asideBtn.addEventListener('click', () => {
-  const logo = document.getElementById('logo');
-  const sidebar = document.getElementById('sidebar');
-  if (logo.style.display === 'none') {
-    logo.style.display = 'block';
-  } else if (logo.style.display === 'block') {
-    logo.style.display = 'none';
-  } else if (sidebar.style.display === 'none') {
-    sidebar.style.display = 'block';
-  } else if (sidebar.style.display === 'block') {
-    sidebar.style.display = 'none';
-  }
+// Botón que maneja la visualización de la barra lateral
+$(document).ready(function() {
+  $('#sidebarCollapse').on('click', function() {
+    $('#sidebar').toggleClass('active');
+  });
 });
-*/
